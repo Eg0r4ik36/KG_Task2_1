e6383511fd4d3cdc4f47a2fda5b156f8414e5a0b
Eg0r4ik36 <ordynskije@bk.ru>
2025-02-25 16:40:25 +0300

    Update DDAAlgorithm.java

--- src/main/java/ru/vsu/cs/kg_task2_1/DDAAlgorithm.java
+++ src/main/java/ru/vsu/cs/kg_task2_1/DDAAlgorithm.java
@@ -21,14 +21,13 @@ public class DDAAlgorithm {
         double y = start.getY();
 
         for (int i = 0; i <= steps; i++) {
-            // Интерполяция цвета
+    
             double t = i / steps;
             double red = startColor.getRed() * (1 - t) + endColor.getRed() * t;
             double green = startColor.getGreen() * (1 - t) + endColor.getGreen() * t;
             double blue = startColor.getBlue() * (1 - t) + endColor.getBlue() * t;
             Color interpolatedColor = new Color(red, green, blue, 1.0);
 
-            // Рисуем пиксель
             gc.setFill(interpolatedColor);
             gc.fillRect(x, y, 1, 1);
 
@@ -36,9 +35,9 @@ public class DDAAlgorithm {
             y += yIncrement;
         }
 
-        // Рисуем круги на концах отрезка для визуализации точек перемещения
+      
         gc.setFill(Color.BLACK);
-        gc.fillOval(start.getX() - 5, start.getY() - 5, 10, 10); // Начало отрезка
-        gc.fillOval(end.getX() - 5, end.getY() - 5, 10, 10); // Конец отрезка
+        gc.fillOval(start.getX() - 5, start.getY() - 5, 10, 10); 
+        gc.fillOval(end.getX() - 5, end.getY() - 5, 10, 10); 
     }
 }
